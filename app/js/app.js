15837c302ac1840c5b6eb9cc501fc83ab865d220
Nikatlas <nikatlas@gmail.com>
2015-10-25 16:06:35 +0200

    Working :P

--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,7 @@
 var app;
 (function (window) {
     'use strict';
-    app = angular.module("heartonia", ["restangular"]);
+    app = angular.module("heartonia", ["restangular", 'infinite-scroll']);
     app.config(function (RestangularProvider) {
         //set the base url for api calls on our RESTful services
         var newBaseUrl = "";
@@ -11,12 +11,29 @@ var app;
             "X-Mashape-Key": "6sZ1LVZK5OmshCU4CTJRbibWFTQsp12KuhZjsnjlxIsM1KoIHh"
         });
         
-        RestangularProvider.setResponseExtractor(function(response, operation) {
+        RestangularProvider.setResponseExtractor(function (response, operation) {
             if (operation === 'getList') {
-                var r = [].concat( response );
+                var r = [].concat(response);
                 return r;
             }
         });
+    }).filter('equals', function() {
+        return function(array, field, search) {
+            if(!search)return array;
+            var matches = [];
+            for(var i = 0; i < array.length; i++) {
+                if( array[i][field] == undefined ){
+                    if( search.length == 0 ){
+                        matches.push(array[i]);   
+                    }
+                }
+                else if (array[i][field].toString() === search.toString() &&
+                    search.length <= array[i][field].toString().length) {
+                    matches.push(array[i]);
+                }
+            }
+            return matches;
+        };
     });
     window.onload = function () {
     };
--- app/js/mainctrl.js
+++ app/js/mainctrl.js
@@ -2,17 +2,41 @@
     'use strict';
     app.controller("mainCtrl", ["$scope", "hearthApi", function (scope, api) {
         scope.cards = [];
-        api.getCards().then(function(c){
-            scope.cards = c[0]["Basic"];
-        });
+        scope.limit = 21;
         
+        api.getCards().then(function(c){
+            scope.cards = [];
+            var cards = [];
+            for(var i in c[0])
+                cards = cards.concat(c[0][i]);
+            var scards = {};
+            for(var i in cards){
+                try{
+                    scards[cards[i].cardId] = cards[i];
+                }
+                catch(e){}
+            }
+            for(var i in scards){
+                scope.cards.push(scards[i]);
+            }
+            scope.limit = 21;               
+        });
+        scope.more = function(){
+            scope.limit += 21;   
+        }
         scope.refresh = function(){
             var filters = {};
             if( scope.mana != "" )filters["cost"] = scope.mana;
             if( scope.attack != "" )filters["attack"] = scope.attack;
             api.getCards(filters).then(function(c){
-                scope.cards = c[0]["Basic"];
+                var cards = [];
+                for(var i in c[0])
+                    cards = cards.concat(c[0][i]);
+                for(var i in cards){
+                   scope.cards[cards[i].cardId] = cards[i];
+                }
             });
+            scope.limit = 21;               
         }
     }]);
 }(app));
